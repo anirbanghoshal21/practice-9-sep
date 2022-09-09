478cf498806accb964bc07fa54c55d4682359544
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-09 22:34:29 +0530

    Add files via upload

---
+++ MuteWithRelease.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    class MutexWithRelease
+    {
+        private static Mutex mutex = new Mutex();
+        const int limits = 1;
+        const int threads = 4;
+
+        static void ThreadsDemo()
+        {
+            for (int i = 0; i < limits; i++)
+            {
+                UseCode();
+            }
+        }
+        static void UseCode()
+        {
+            mutex.WaitOne();
+            Console.WriteLine("{0} has entered in the code", Thread.CurrentThread.Name);
+            Console.WriteLine("{0} is leaving the code\r\n", Thread.CurrentThread.Name);
+            mutex.ReleaseMutex();
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < threads; i++)
+            {
+                Thread t = new Thread(new ThreadStart(ThreadsDemo));
+                t.Name = String.Format("Thread{0}", i + 1);
+                t.Start();
+            }
+            Console.Read();
+
+        }
+    }
+}
