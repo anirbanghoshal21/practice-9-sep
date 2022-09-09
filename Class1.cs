408420eb12aef50329e3bdba9dcc45e6b587b8a9
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-09 20:52:10 +0530

    prob 2

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace Multithreading
+{
+    class IsALive
+    {
+        static void M1()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Sleep State =" + Thread.CurrentThread.ThreadState);
+        }
+
+        static void M2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Print  i{0} ", i);
+                Console.WriteLine("Thread name " + Thread.CurrentThread.Name);
+
+                Thread.Sleep(200);
+            }
+
+        }
+        
+
+        }
+    }
